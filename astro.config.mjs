e200dd21740d485e4f99121abb7c70247538ea36
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-01-24 09:32:19 +0900

    feat: 基礎を作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'http://localhost:4321', 
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: true, // Tailwind 
+      },
+    }),
+  ],
+});
