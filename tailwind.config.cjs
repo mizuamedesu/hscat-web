5834060322659a3db0ba6a5e3b590d2deb4bd757
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-01-24 20:37:02 +0900

    feat: マークダウンへのスタイルを当てました

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      "./src/**/*.{astro,html,js,jsx,tsx,vue,svelte}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          // 例: ブランディングカラー等あればここで追加
-          primary: "#4B9CD3",
-          secondary: "#FFAF00"
-        }
+  content: [
+    "./src/**/*.{astro,html,js,jsx,tsx,vue,svelte}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4B9CD3",
+        secondary: "#FFAF00"
       }
-    },
-    plugins: [],
-  };
-  
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+};
