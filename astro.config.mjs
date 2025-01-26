f5e846e6c796d1ad4a71d46bcb9600fe3646c365
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-01-26 11:57:38 +0900

    feat: head系のタグをちゃんと設定してSEO対策をゴリゴリに

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'http://localhost:4321', 
+  site: 'https://hscat.org/', 
   integrations: [
     tailwind({
       config: {
