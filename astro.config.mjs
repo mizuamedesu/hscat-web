e4dff17cff0315d1df0f5a0f2abc248ec3ae7d01
mizuamedesu <97249122+mizuamedesu@users.noreply.github.com>
2025-01-26 22:12:11 +0900

    feat: sitemapをサポート

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://hscat.org/', 
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: true, // Tailwind 
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: true, // Tailwind 
+    },
+  }), sitemap()],
 });
